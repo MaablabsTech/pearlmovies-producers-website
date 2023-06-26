1821e900c309d52f7919cdb707e6ed5c6701ce3e
keyxyz <135964407+keyxyz@users.noreply.github.com>
2023-06-26 18:11:40 +0300

    added home.slider route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home.index');
 });
+
+Route::get('/home_slider', function () {
+    return view('home.slider');
+});
+
+
