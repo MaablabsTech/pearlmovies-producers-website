a34952eeeee969dd78f48a81b6449e6e7cbe79fa
keyxyz <135964407+keyxyz@users.noreply.github.com>
2023-07-11 21:10:32 +0000

    update data passed to home slider view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
         ];
 
         if ($setting->home_page_style === 'slider') {
-            return view('home.index_slider')->with($data);
+            return view('home.index_slider', ['data' => $data, 'featured_movies' => $featured_movies]);
         } else {
             return view('home.index')->with($data);
         }
