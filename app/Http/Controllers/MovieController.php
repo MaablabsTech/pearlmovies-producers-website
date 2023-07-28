8fbd18478620bc9ded262b885cad6c783ce9ea82
keyxyz <135964407+keyxyz@users.noreply.github.com>
2023-07-28 05:10:05 -0500

    Update MovieController.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -14,11 +14,7 @@ class MovieController extends Controller
         if (!is_null($movie)) {
             return view('movie.detail')->with($movie);
         } else {
-<<<<<<< HEAD
-            redirect('/');
-=======
-            return redirect()->route('home');
->>>>>>> 73f3639896008c9ec7f871848018ccee2fa6b048
+            return redirect('/');
         }
     }
 }
